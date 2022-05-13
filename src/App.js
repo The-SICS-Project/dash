4d1fa63c9543b6cec958416aa4c2029b190810c8
varun k v <varunkv20145@gmail.com>
2022-05-13 14:02:33 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -11,12 +11,13 @@ function Login() {
           </Helmet>
           <img className="Logo" src={Logo} id={Logo} width="150" height="120"/><br/><br/>
         <form>
-
-          <input type="text" placeholder="Email address" id ="Email" name="Email" /><br></br><br></br>
-          <input type="password" placeholder="password" name="password"  /><br></br><br></br>
+           <input type="email" placeholder="Email address" id ="Email" name="Email" required /><br></br><br></br>
+          <input type="password" placeholder="password" name="password" required  /><br></br><br></br>
           <button class="login" type="submit">Login</button><br></br><br></br>
 
+
           <span class="sign">Don't have an account? <a href="#">sign up</a></span>
+
         </form>
 
       </div>
