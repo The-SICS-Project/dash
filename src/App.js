79fd40ca0ddd07af1435fe437bb109eaaaf23905
varun k v <varunkv20145@gmail.com>
2022-05-15 13:28:16 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import Logo from './Logo.svg'
 import { Helmet } from 'react-helmet';
+import {Magic} from 'magic-sdk';
+const m = new Magic('pk_live_AABC93231E65FC6F');
+ const handleLogin=async({ email})=>{
 
+     try {
+        const didToken= await m.auth.loginWithMagicLink({ email:document.getElementById('email').value });
+         console.log(didToken);
+     } catch {
+     }
+
+}
 function Login() {
+
+
+
   return (
 
-      <div class="container">
+      <div className="container">
           <Helmet>
           <title>SICS | Login</title>
               <link rel="icon" href="Logo.svg" type="image/svg"/>
           </Helmet>
           <img className="Logo" src={Logo} id={Logo} width="150" height="120"/><br/><br/>
-        <form>
-           <input type="email" placeholder="Email address" id ="Email" name="Email" required /><br></br><br></br>
+        <form >
+
+
+           <input className="Email" type="email" placeholder="Email"  id="Email" required /><br></br><br></br>
           <input type="password" placeholder="password" name="password" required  /><br></br><br></br>
-          <button class="login" type="submit">Login</button><br></br><br></br>
+          <button className="login" type="submit" onClick={handleLogin}>Login</button><br></br><br></br>
 
 
-          <span class="sign">Don't have an account? <a href="#">sign up</a></span>
+          <span className="sign">Don't have an account? <a href="#">sign up</a></span>
 
         </form>
 
@@ -24,5 +39,6 @@ function Login() {
 
 
   )
+
 }
 export default Login;
