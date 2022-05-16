7c51f11b906c847cf3dc3cc71920e895e0660f68
varun k v <varunkv20145@gmail.com>
2022-05-16 21:01:04 +0530

    Modified package.json to add CI=false

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import {Magic} from 'magic-sdk';
-import  ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import Login from './App.js'
 import "./styles.css";
-const m = new Magic('pk_live_AABC93231E65FC6F');
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Login />);
+root.render(<Login/>);
+process.env.CI = false;
