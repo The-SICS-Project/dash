6fd15beedfc63a3f9413fad0e7a7eb69a04f36d4
VARUNKV20145 <56473185+VARUNKV20145@users.noreply.github.com>
2022-07-16 21:41:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Login from "./Login";
+import Signup from "./signup";
+
+function App() {
+    return (
+
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login/>}/>
+                <Route path="/signup" element={<Signup/>}/>
+
+            </Routes>
+        </Router>
+
+    )
+
+}
+
+export default App;
