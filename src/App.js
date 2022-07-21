022fc177cdb49fce224f76621b8c2b4afe58ff81
Vishnu Nair P <58590215+VishnuNairP@users.noreply.github.com>
2022-07-21 15:30:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Login from "./Login";
+import Signup from "./signup";
+import Ticket from "./Ticket";
+import Userdashboard from "./Userdashboard";
+import React, { useState, useEffect } from 'react';
+import {UserContext} from "./userContext";
+import { checkUser } from './Magic';
+import PrivateRoute from './PrivateRoute';
+
+const App=()=> {
+    const [user, setUser] = useState({ isLoggedIn: null, email: '' });
+    const [loading, setLoading] = useState();
+    useEffect(() => {
+        const validateUser = async () => {
+            setLoading(true);
+            try {
+                await checkUser(setUser);
+                setLoading(false);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        validateUser();
+    }, [user.isLoggedIn]);
+        return (
+            <UserContext.Provider value={user}>
+        <Router>
+            {user.isLoggedIn && <navigate to={{ pathname: '/dashboard' }}/>}
+            <Routes>
+                <Route path="/" element={<Login/>}/>
+                <Route path="/signup" element={<Signup/>}/>
+                <Route path="/dashboard" element={<Userdashboard/>} />
+                    <Route path="/dashboard/ticket" element={<Ticket/>}/>
+            </Routes>
+        </Router>
+            </UserContext.Provider>
+    )
+
+}
+
+export default App;
---
+++ src/Userdashboard.js
@@ -0,0 +1,30 @@
+import Home from "./Home";
+import Navbar from "./Navbar";
+import './user_dashboard.css';
+import Ticket from "./Ticket";
+import Chat from "./Chat";
+import {Route, Router, Routes,useNavigate} from "react-router-dom";
+import { UserContext } from './userContext';
+import { logoutUser } from './Magic';
+import {useContext} from "react";
+
+
+
+const Userdashboard=()=>{
+
+
+
+       return(
+        <>
+        <Navbar />
+    <div className="Container">
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/dashboard/ticket" element={<Ticket/>}/>
+            <Route path="dashboard/chat" element={<Chat/>}/>
+        </Routes>
+    </div>
+            </>
+    )
+}
+ export default Userdashboard;
